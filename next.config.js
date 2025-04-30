3129915b289343dce9b3cb17d95ad7567a112b06
Biny Adenew <binyhood58@gmail.com>
2025-04-30 23:33:47 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Don't stop the build when there are ESLint errors 
+    // in production - only show warnings
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['*'], // This is insecure - you should specify actual domains
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true, // For Netlify static deployment
+  },
+}
+
+module.exports = nextConfig
